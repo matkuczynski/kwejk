03425bf776c463490bbfb3c30e595a83ec50b90b
Kamil <milek111@yahoo.com>
2017-08-02 17:42:39 +0200

    added gif search

--- src/main/java/pl/akademiakodu/kwejk/controller/GifController.java
+++ src/main/java/pl/akademiakodu/kwejk/controller/GifController.java
@@ -3,8 +3,11 @@ package pl.akademiakodu.kwejk.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import pl.akademiakodu.kwejk.dao.GifDaoImpl;
+import pl.akademiakodu.kwejk.model.Gif;
 
 @Controller
 public class GifController {
@@ -26,7 +29,12 @@ public class GifController {
     @GetMapping("/gif/{name}")
     public String display(@PathVariable String name, ModelMap modelMap ){    //@PathVariable - odnosi sie bezposrednio do id url
         modelMap.addAttribute("gifs", gifDaoImpl.findByName(name));
-        return "chosen-gif";  // wyswietlac osobe o odpowiednim id
+        return "chosen-gif";
     }
 
+    @GetMapping("/gifs/search")
+    public String search(@RequestParam String gifName, ModelMap modelMap) {
+        modelMap.addAttribute("gifs", gifDaoImpl.findByName(gifName));
+        return "home";
+    }
 }
--- src/main/java/pl/akademiakodu/kwejk/dao/GifDaoImpl.java
+++ src/main/java/pl/akademiakodu/kwejk/dao/GifDaoImpl.java
@@ -42,5 +42,4 @@ public class GifDaoImpl implements GifDaoInterface {
         }
         return result;
     }
-
 }
