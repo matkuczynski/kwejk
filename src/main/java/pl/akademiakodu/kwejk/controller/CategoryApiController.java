315722a22457e4e0b3c4923aa66710def5961500
Kamil <milek111@yahoo.com>
2017-08-03 17:18:16 +0200

    added  /api/{gifname}

--- src/main/java/pl/akademiakodu/kwejk/controller/CategoryApiController.java
+++ src/main/java/pl/akademiakodu/kwejk/controller/CategoryApiController.java
@@ -1,17 +1,29 @@
 package pl.akademiakodu.kwejk.controller;
 
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import pl.akademiakodu.kwejk.dao.CategoryDao;
+import pl.akademiakodu.kwejk.dao.GifDaoImpl;
 import pl.akademiakodu.kwejk.model.Category;
+import pl.akademiakodu.kwejk.model.Gif;
 
 import java.util.List;
 
 @RestController
 public class CategoryApiController {
+
+    private GifDaoImpl gifDaoImpl = new GifDaoImpl();
+
     @GetMapping("/api/categories")
     public List<Category> listCategories(){
         CategoryDao categoryDao = new CategoryDao();
         return categoryDao.showAllCategories();
+    }
+
+    @GetMapping("/api/{name}")
+    public List<Gif> apiGifDetails(@PathVariable String name){
+        return gifDaoImpl.findByName(name);
     }
 }
